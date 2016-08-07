59e5a2551301e262d3ec422e532cad190ce2bc85
Paul Campbell <paulcampbell@pcampbellHLlaptop.local>
2016-08-07 17:34:05 -0500

    apply layout to 3 elements, and create UI elements, closes #7, closes #6, closes #5, closes #4

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: AnyObject) {
+        print ("record button was pressed")
+        recordingLabel.text = "Recording In Progress..."
+    }
+
+
+    @IBAction func stopRecording(sender: AnyObject) {
+    }
+    
 
 }
 
